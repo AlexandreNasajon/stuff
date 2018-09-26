a14064313adeaf0a640dd2c7008b7a8a395b9787
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-09-26 15:34:38 -0300

    Update els.py

--- els.py
+++ els.py
@@ -1,7 +1,7 @@
 def els(origem , i , j , text , word , n , k):
-    if i<= len(text):
+    if i <= len(text)-1:
         if text[i] == word[j]:
-            if j == len(word):
+            if j == len(word)-1:
                 return els(0 , 0 , 0 , text[1:] , word , n , k+1)
             else:
                 return els(origem , i+n+1 , j+1 , text , word , n , k)
