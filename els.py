4003a82b1eb600fcd2b4329c369e71904c8ba515
nasajon-chan <alexandrenasajon66@hotmail.com>
2018-09-26 15:23:26 -0300

    Create els.py

---
+++ els.py
@@ -0,0 +1,17 @@
+def els(origem , i , j , text , word , n , k):
+    if i<= len(text):
+        if text[i] == word[j]:
+            if j == len(word):
+                return els(0 , 0 , 0 , text[1:] , word , n , k+1)
+            else:
+                return els(origem , i+n+1 , j+1 , text , word , n , k)
+        else:
+            return els(origem , origem+1 , 0 , text , word , n , k)
+    else:
+        return k
+
+print(els(0,0,0,"a","a",0,0,)==1)
+print(els(0,0,0,"ab","a",0,0,)==1)
+print(els(0,0,0,"abcde","ace",1,0,)==1)
+print(els(0,0,0,"ababacab","ab",0,0,)==3)
+print(els(0,0,0,"ababab","a",1,0,)==3)
